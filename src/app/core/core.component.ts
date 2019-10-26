d12ddf6a9a1d4e63374c773fb1a9964e5958962e
Kevin Qu <kevinqu88@gmail.com>
2019-10-26 19:51:19 -0400

    start

--- src/app/core/core.component.ts
+++ src/app/core/core.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { LoginService } from '../shared/services/user.service';
+import { ToastrService } from 'ngx-toastr';
+import { User } from '../shared/model/user.model';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { map, switchMap } from 'rxjs/operators';
+import * as _ from 'lodash';
+import * as FUTILS from '../shared/utils/forms.utils';
 
 @Component({
   selector: 'app-core',
@@ -7,9 +15,16 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class CoreComponent implements OnInit {
-  constructor() {
+
+  constructor(public router: Router, public route: ActivatedRoute) {
 
   }
 
-  ngOnInit() { }
+  ngOnInit() {
+
+  }
+
+  onNewSeason(): void {
+
+  }
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CoreComponent } from './core.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModules } from '../shared/material-module';
 import { FeedComponent } from './feed/feed.component';
+import { PipesBarrelModule } from '../shared/pipes/pipes.module';
 
 @NgModule({
   imports: [
-
+    RouterModule,
+    MaterialModules,
+    PipesBarrelModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
 
   exports: [
