0bb1a859214af58527e5109528e4084fac40388a
Kevin Qu <kevinqu88@gmail.com>
2019-10-29 21:46:09 -0400

    ye

--- src/app/shared/dialogs/new-season/new-season.component.ts
+++ src/app/shared/dialogs/new-season/new-season.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit, OnDestroy, Inject } from '@angular/core';
+import { Component, OnInit, OnDestroy, Inject, ViewChild } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatStepper } from '@angular/material/stepper'; 
 import { MatButton } from '@angular/material/button';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators, FormControl, AbstractControl} from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { HttpResponse } from '@angular/common/http';
 import { take, map, switchMap, exhaustMap, concatMap, tap, 
   takeUntil, mergeMap } from 'rxjs/operators';
 import { CrudRestServie } from '../../../shared/services/crud.service';
 import { LoginService } from '../../services/user.service';
+import * as FUTILS from '../../../shared/utils/forms.utils';
+import * as VALS from '../../../shared/validators/general-validators'
 
 @Component({
   selector: 'app-dialog-new-season',
@@ -17,13 +20,28 @@ import { LoginService } from '../../services/user.service';
 
 export class NewSeasonComponent implements OnInit, OnDestroy {
 
+  @ViewChild('seasonStepper', {static: false})
+  seasonStepper: MatStepper
+
   title: string = "Add New Season";
   subTitle: string = "There are currently ";
   subTitleSuffix: string = " on going seasons. Would you like to start a new one?";
   inputData: any;
 
-  firstFormGroup: FormGroup;
-  secondFormGroup: FormGroup;
+  inputFg: FormGroup;
+  steps: StepperObj[] = [];
+
+  get player1Ctrl(): AbstractControl {
+    return this.inputFg.get('player1Id');
+  }
+
+  get player2Ctrl(): AbstractControl {
+    return this.inputFg.get('player2Id');
+  }
+
+  get gamesCount(): AbstractControl {
+    return this.inputFg.get('gamesCount');
+  }
 
   constructor(public ls: LoginService, public cs: CrudRestServie,
     public dialogRef: MatDialogRef<NewSeasonComponent>,
@@ -37,16 +55,52 @@ export class NewSeasonComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this.firstFormGroup = this.fb.group({
-      firstCtrl: ['', Validators.required]
+    this.constructStepperObj();
+
+    this.inputFg = this.fb.group({
+      player1Id: FUTILS.createFormControl('John', false, []),
+      player2Id: FUTILS.createFormControl('Jake', false, [Validators.required]),
+      gamesCount: FUTILS.createFormControl(8, false, [Validators.required]),
+      review: FUTILS.createFormControl(null, true),
     });
-    this.secondFormGroup = this.fb.group({
-      secondCtrl: ['', Validators.required]
-    });
+
+    this.inputFg.valueChanges.subscribe((res) => {
+      console.log(this.inputFg.getRawValue())
+    })
 
   }
 
+  constructStepperObj() {
+    this.steps.push(
+      new StepperObj("Player One", 'ID/Name', 'Select a player', 'player1Id'),
+      new StepperObj("Player Two", 'ID/Name', 'Select a player', 'player2Id'),
+      new StepperObj("Total Games Count", 'ID/Name', 'Total amount of games in this season', 'gamesCount'),
+      new StepperObj("Review", null, null, "review")
+    )
+  }
+
+  onNext(isLastStep: boolean, index: number) {
+    console.log(isLastStep, index);
+    this.seasonStepper.next();
+  }
+
+  onBack(index: number) {
+    console.log(index)
+    this.seasonStepper.previous();
+  }
+
   ngOnDestroy() {
+
+  }
+}
+
+export class StepperObj {
+  constructor(public labelName: string, 
+    public phDisplay: string, 
+    public hint: string,
+    public formCtrlName: string,
+    public buttonNextDisplay: string = "Next",
+    public buttonBackDisplay: string = "Back") {
 
   }
 }
