955037242d733bc75f0a2ed00c3be8924121de7c
Kevin <kevinqu88@gmail.com>
2020-01-03 17:26:51 -0500

    scorez

--- src/app/core/feed/season/types/edit/edit.component.ts
+++ src/app/core/feed/season/types/edit/edit.component.ts
@@ -10,6 +10,7 @@ import { InitErrorMatcher } from '../../../../../shared/matchers/form-err-matche
 export const INPUT_FORMAT: string = "MM/DD/YY HH:mm";
 export const INPUT_TYPES: string[] = [INPUT_FORMAT, "MM/DD/YYYY HH:mm", "MM-DD-YY HH:mm"];
 export const DISPLAY_FORMAT: string = "MM/DD/YY hh:mm a";
+export const POSITIVE_INT_PATTERN: RegExp = new RegExp(/^[1-9]\d*$/);
 
 @Component({
     selector: 'season-game-editor',
@@ -29,14 +30,23 @@ export class SeasonGameEditComponent implements OnInit {
     datePlayedTitle: string = "";
     gameStatusTitle: string = "";
     gameFg: FormGroup;
+    datePlayedHint: string = "Enter in format: MM/DD/YY HH:mm";
+    dateplayedPlaceholder: string = "Game played date";
     initErrorMatcher = new InitErrorMatcher();
 
     constructor(public fb: FormBuilder) {
-
     }
 
     get datePlayedControl(): FormControl {
       return <FormControl>this.gameFg.get("datePlayed");
+    }
+
+    get finished(): FormControl {
+      return <FormControl>this.gameFg.get("finished");
+    }
+
+    get gameControllers(): FormArray {
+      return <FormArray>this.gameFg.get("controllers");
     }
 
     ngOnInit() {
@@ -53,8 +63,9 @@ export class SeasonGameEditComponent implements OnInit {
      * Note: You can define the control with just the initial value, 
      * but if your controls need sync or async validation, 
      * add sync and async validators as the second and third items in the array.
-     */
-
+    * 
+    * Create Form Group for the game being edited.
+    */
     createGameFormGroup() {
       this.gameFg = this.fb.group({
         finished: FUTILS.createFormControl2(this.game.finished, false),
@@ -66,29 +77,29 @@ export class SeasonGameEditComponent implements OnInit {
       //loop through controllers
       this.game.controllers.forEach((controller: IGameController, index: number) => {
         // loop through goal details and create array
-        //const goalScoredFga = this.fb.array();
-
+        const goalScoredFga: FormArray = this.fb.array([]);
+        controller.goalDetails.forEach((goalDet: GoalDetail) => {
+          goalScoredFga.push(new FormGroup({
+            goalCount: FUTILS.createFormControl2(goalDet.goalCount, false, 
+              [Validators.required, Validators.pattern(POSITIVE_INT_PATTERN)]),
+            goalScorer: FUTILS.createFormControl2(goalDet.goalScorer, false, [Validators.required]),
+            goalTime: FUTILS.createFormControl2(goalDet.goalTime, false, [Validators.required])
+          }))
+        });
         // create the individual controller fg
         const controllerFg = this.fb.group({
-          goalsScored: FUTILS.createFormControl2(this.game.controllers[index].goalsScored, false, [
-            Validators.required, Validators.min(0)]),
-          teamName: FUTILS.createFormControl2(this.game.controllers[index].teamName, true),
-          user: FUTILS.createFormControl2(this.game.controllers[index].user, true)
+          goalDetails: goalScoredFga,
+          goalsScored: FUTILS.createFormControl2(this.game.controllers[index].goalsScored, true, [
+            Validators.required]),
+          teamName: FUTILS.createFormControl2(this.game.controllers[index].teamName, false, [Validators.required]),
+          user: FUTILS.createFormControl2(this.game.controllers[index].user, false, [Validators.required])
         });
-
         // push the fg to controller form array
         (<FormArray>this.gameFg.get("controllers")).push(controllerFg);
       });
       console.log(this.gameFg)
     }
 
-    createFgFromGoalDetail(detail: GoalDetail): FormGroup {
-      return this.fb.group({
-        goalCount: FUTILS.createFormControl2(detail.goalCount, false, [Validators.required]),
-        goalScorer: FUTILS.createFormControl2(detail.goalScorer, false, [Validators.required]),
-        goalTime: FUTILS.createFormControl2(detail.goalTime, false, [Validators.required])
-      })
-    }
 
     updateTitle() {
       const datePlayed: string = moment(this.game.datePlayed).format(DISPLAY_FORMAT);
@@ -106,5 +117,9 @@ export class SeasonGameEditComponent implements OnInit {
 
 
       //this.onGameSaved.emit();
+    }
+
+    getGameController(i: number): FormGroup {
+      return <FormGroup>(<FormArray>this.gameFg.get("controllers")).at(i);
     }
 }
